aacfea3211343d1bf1a14d37778e8876df01716a
MaxKnyazev <max.knyazev77@gmail.com>
2020-07-24 13:52:04 +0300

    Updates

---
+++ precache-manifest.95cf8a1ceddb51a395678825b7adf96d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91a0b0162d9d4aeeb4eeae7084a7535b",
+    "url": "/accent-reactjs/index.html"
+  },
+  {
+    "revision": "73b025025434ac1590c0",
+    "url": "/accent-reactjs/static/css/main.c87d259c.chunk.css"
+  },
+  {
+    "revision": "0ce5f5c8557d0e9349f9",
+    "url": "/accent-reactjs/static/js/2.d4aef070.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/accent-reactjs/static/js/2.d4aef070.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "73b025025434ac1590c0",
+    "url": "/accent-reactjs/static/js/main.f79ec852.chunk.js"
+  },
+  {
+    "revision": "18769881859a80f88c75",
+    "url": "/accent-reactjs/static/js/runtime-main.4a6d4c03.js"
+  }
+]);
